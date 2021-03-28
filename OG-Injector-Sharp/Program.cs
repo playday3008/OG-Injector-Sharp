4c369b0048d93900a0b8a4385e8bbbb566067b7b
PlayDay <18056374+playday3008@users.noreply.github.com>
2021-03-28 12:19:54 +0200

    Update

--- OG-Injector-Sharp/Program.cs
+++ OG-Injector-Sharp/Program.cs
@@ -76,72 +76,6 @@ namespace OGInjector
         }
     }
 
-    class WinInject
-    {
-        public static bool Inject(Process process, string processName, string libraryPath)
-        {
-            IntPtr allocatedMem = WinAPI.VirtualAllocEx(process.Handle, IntPtr.Zero, (uint)Encoding.Unicode.GetBytes(libraryPath).Length + 1, WinAPI.AllocationType.MEM_RESERVE | WinAPI.AllocationType.MEM_COMMIT, WinAPI.MemoryProtection.PAGE_READWRITE);
-            if (allocatedMem == IntPtr.Zero)
-            {
-                Color.DarkRed(); Console.Write("Can't allocate memory in ");
-                Color.Red(); Console.Write(processName);
-                Color.DarkRed(); Console.WriteLine(" to write");
-                Console.ResetColor();
-                Console.WriteLine("Catched error code: " + Marshal.GetLastWin32Error());
-                Color.White(); Console.WriteLine("Press any key to continue...");
-                Console.ResetColor();
-                Console.ReadKey();
-                return false;
-            }
-            if (!WinAPI.WriteProcessMemory(process.Handle, allocatedMem, Encoding.Unicode.GetBytes(libraryPath), (uint)(uint)Encoding.Unicode.GetBytes(libraryPath).Length + 1, out _))
-            {
-                Color.DarkRed(); Console.Write("Can't write dll path to ");
-                Color.Red(); Console.WriteLine(processName);
-                Console.ResetColor();
-                Console.WriteLine("Catched error code: " + Marshal.GetLastWin32Error());
-                Color.White(); Console.WriteLine("Press any key to continue...");
-                Console.ResetColor();
-                Console.ReadKey();
-                return false;
-            }
-            IntPtr kernel32 = WinAPI.GetModuleHandleW("kernel32.dll");
-            if (kernel32 == IntPtr.Zero)
-            {
-                Color.DarkRed(); Console.Write("Can't get kernel32.dll handle");
-                Console.ResetColor();
-                Console.WriteLine("Catched error code: " + Marshal.GetLastWin32Error());
-                Color.White(); Console.WriteLine("Press any key to continue...");
-                Console.ResetColor();
-                Console.ReadKey();
-                return false;
-            }
-            IntPtr loadLibraryAddr = WinAPI.GetProcAddress(kernel32, "LoadLibraryW");
-            if (loadLibraryAddr == IntPtr.Zero)
-            {
-                Color.DarkRed(); Console.Write("Can't get LoadLibraryW address from kernel32.dll");
-                Console.ResetColor();
-                Console.WriteLine("Catched error code: " + Marshal.GetLastWin32Error());
-                Color.White(); Console.WriteLine("Press any key to continue...");
-                Console.ResetColor();
-                Console.ReadKey();
-                return false;
-            }
-            IntPtr thread = WinAPI.CreateRemoteThread(process.Handle, IntPtr.Zero, 0, loadLibraryAddr, allocatedMem, 0, out _);
-            if (thread == IntPtr.Zero)
-            {
-                Color.DarkRed(); Console.Write("Can't create remote thread with LoadLibrary module in ");
-                Color.Red(); Console.WriteLine(processName);
-                Console.ResetColor();
-                Console.WriteLine("Catched error code: " + Marshal.GetLastWin32Error());
-                Color.White(); Console.WriteLine("Press any key to continue...");
-                Console.ResetColor();
-                Console.ReadKey();
-                return false;
-            }
-            return true;
-        }
-    }
-
     class Program
     {
         private static bool Bypass(Process process, string processName)
@@ -312,7 +246,12 @@ namespace OGInjector
             Console.ResetColor();
 
             if (!WinInject.Inject(processes[^1], processName, libraryPath))
+            {
+                Color.White(); Console.WriteLine("Press any key to continue...");
+                Console.ResetColor();
+                Console.ReadKey();
                 return 1;
+            }
 
             Color.DarkGreen();  Console.Write("Successfully injected ");
             Color.Cyan();       Console.Write(libraryName);
